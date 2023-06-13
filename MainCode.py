dae6806ac051093b68b5ba08c7a45e9292ed0b05
20103 <20103@student.macleans.school.nz>
2023-06-14 11:42:26 +1200

    Basic introduction and user information

---
+++ MainCode.py
@@ -0,0 +1,17 @@
+#Array to store the names of players (string).
+playerNames = []
+
+#Initial prompt & greeting.
+print("Hello! This is a fun trivia quiz game that will consist of varying questions and genres. There is a maximum of 4 players.")
+
+numOfPlayers = int(input("Please type how many people will be playing (e.g. 2): ").strip())
+
+#Ask user again if the input is above 4 or below 1.
+while numOfPlayers > 4 or numOfPlayers < 1:
+    numOfPlayers = int(input("Sorry, but that is not a valid answer! Please type how many people will be playing (e.g. 2): "))
+
+#You can consider adding a "Confirm?" prompt to the user, however this is not essential.
+for i in range(numOfPlayers):
+    name = input("Please enter a name Player {}: ".format(i+1)).strip()
+    playerNames.append(name)
+    print("Welcome, {}!".format(name))
