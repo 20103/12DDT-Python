f4149adf3ec2c9a0b4a09ff5dda8f33a06f5cb41
20103 <20103@student.macleans.school.nz>
2023-08-10 10:36:34 +1200

    added more questions, looped game, colorama, etc.

--- MainCode.py
+++ MainCode.py
@@ -1,14 +1,16 @@
-import csv, random, colorama
+import csv, random, math
 
-from colorama import Fore
-colorama.init(autoreset=True)
+from colorama import Fore, init
+init(autoreset=True) #auto reset won't work for some mysterious reason
 
 #consider adding tkinter gui if i can be bothered
+#also maybe add option for multiple different answers including numerical. can be done through lists, separating them at commas etc.
 
 with open("./questions.csv", "r", encoding='utf-8') as csvfile:
     reader_variable = csv.reader(csvfile, delimiter=",")
     for row in reader_variable:
         data = list(reader_variable)
+
 
 #CONFIG
 QUESTION_AMOUNT = 10 #How many trivia questions per game
@@ -17,11 +19,9 @@ QUESTION_AMOUNT = 10 #How many trivia questions per game
 questionInfo = None
 usedQuestions = []
 
-playerScore = 0
-
 numOfQuestions = len(data) #usable with arrays
 
-#print("DEBUGGING: The variable 'data' has {} trivia questions.".format(len(data)))
+print("DEBUGGING: The variable 'data' has {} trivia questions.".format(len(data)))
 
 #Test run!
 
@@ -35,67 +35,95 @@ numOfQuestions = len(data) #usable with arrays
 
 #This function was definitely a highlight for me. I learnt list comprehension and how useful they can be. https://www.w3schools.com/python/python_lists_comprehension.asp
 def generateQuestion():
+    #takes a question from data list and assigned to availableQuestions if not already present
     availableQuestions = [q for q in data if q not in usedQuestions]
     if len(availableQuestions) == 0:
-        print("ERROR: NO MORE QUESTIONS AVAILABLE")
+        print(Fore.LIGHTRED_EX + "ERROR: NO MORE QUESTIONS AVAILABLE")
         exit()
     randomNumber = random.randint(0, len(availableQuestions) - 1)
     selectedQuestion = availableQuestions[randomNumber]
     usedQuestions.append(selectedQuestion)
     return selectedQuestion
 
+def runGame():
+    playerScore = 0
 
-#Initial prompt & greeting.
-print("Hello! This is a fun trivia quiz game that will consist of varying questions and genres.")
+    #Initial prompt & greeting.
+    print(Fore.WHITE + "Hello! This is a fun trivia quiz game that will consist of varying questions and genres.")
+    input(Fore.LIGHTBLACK_EX + "Press Enter to continue..." + Fore.LIGHTCYAN_EX)
 
-#You can consider adding a "Confirm?" prompt to the user, however this is not essential.
-nameInput = input("Please enter a username: ")
+    #You can consider adding a "Confirm?" prompt to the user, however this is not essential.
+    nameInput = input(Fore.LIGHTYELLOW_EX + "Please enter a username: " + Fore.LIGHTCYAN_EX)
 
-while nameInput.isspace():
-    nameInput = input("You did not enter anything! Please enter a username: ")
+    while nameInput.isspace() or nameInput == "":
+        nameInput = input(Fore.RED + "You did not enter anything! Please enter a username: " + Fore.LIGHTCYAN_EX)
 
-playerName = nameInput.strip()
-print(f"Welcome, {playerName}!")
+    playerName = nameInput.strip()
+    print(f"{Fore.WHITE}Welcome, {playerName}!")
+    input(Fore.LIGHTBLACK_EX + "Press Enter to continue..." + Fore.LIGHTCYAN_EX)
 
-#Main game
-print(f"There is a total of {QUESTION_AMOUNT} trivia questions that must be answered. You will be given a score out of {QUESTION_AMOUNT}.\n ALL NUMERICAL ANSWERS ONLY ACCEPT DIGITS, DO NOT USE WORD FORM!")
+    #Main game
+    print(f"{Fore.WHITE}There is a total of {QUESTION_AMOUNT} trivia questions that must be answered. You will be given a score out of {QUESTION_AMOUNT}.\nALL NUMERICAL ANSWERS ONLY ACCEPT DIGITS, DO NOT USE WORD FORM!")
+    input(Fore.LIGHTBLACK_EX + "Press Enter to continue..." + Fore.LIGHTCYAN_EX)
 
-for i in range(1, QUESTION_AMOUNT + 1):
-    #Get question & data. Store into appropriate variables.
-    questionInfo = generateQuestion()
-    topic = questionInfo[0]
-    question = questionInfo[1]
-    answer = questionInfo[2].lower()
-    desc = questionInfo[3]
+    for i in range(1, QUESTION_AMOUNT + 1):
+        #Get question & data. Store into appropriate variables.
+        questionInfo = generateQuestion() #See generateQuestion() function above for more info
+        topic = questionInfo[0]
+        question = questionInfo[1]
+        answer = questionInfo[2].lower()
+        desc = questionInfo[3]
 
-    answerInput = str(input(Fore.YELLOW + f"Genre: {topic}\n" + f"Question {i}: {question} ")).lower()
+        answerInput = str(input(Fore.LIGHTYELLOW_EX + f"Genre: {topic}\n" + f"Question {i}: {question} " + Fore.LIGHTCYAN_EX)).lower()
 
-    if answerInput == answer:
-        print(Fore.GREEN + "Correct!")
-        playerScore += 1
+        if answerInput == answer:
+            print(Fore.LIGHTGREEN_EX + "Correct!")
+            playerScore += 1
+        else:
+            print(Fore.LIGHTRED_EX + f"Incorrect! The answer was: {answer}")
+        print(desc)
+        input(Fore.LIGHTBLACK_EX + "Press Enter to continue..." + Fore.LIGHTCYAN_EX)
+
+    return(playerScore, playerName)
+            
+def endScreen(playerInfo):
+    #For readability
+    playerScore = playerInfo[0]
+    playerName = playerInfo[1]
+
+    #Endgame
+    percentageScore = math.floor(playerScore / QUESTION_AMOUNT * 100)
+
+    #get percentage by dividing playerscore with QUESTION_AMOUNT
+
+    print(f"{Fore.YELLOW}Well done, {playerName}! You have answered all the questions.\nYour total score is: {playerScore} out of {QUESTION_AMOUNT}. This means you got {percentageScore}% of the questions correct!")
+    input(Fore.LIGHTBLACK_EX + "Press Enter to continue..." + Fore.LIGHTCYAN_EX)
+    if percentageScore == 100:
+        print(Fore.GREEN + "Wow, that is impressive! You answered every question correctly.")
+    elif percentageScore >= 80:
+        print(Fore.LIGHTGREEN_EX + "Pretty good! You got the majority of questions correct.")
+    elif percentageScore >= 60:
+        print(Fore.YELLOW + "Not bad! You've answered more questions correctly than incorrectly.")
+    elif percentageScore >= 40:
+        print(Fore.LIGHTRED_EX + "Not the best score, but there is definitely room for improvement!")
     else:
-        print(Fore.RED + f"Incorrect! The answer was: {answer}")
-    print(desc)
-        
-#Endgame
-percentageScore = playerScore / QUESTION_AMOUNT * 100
+        print(Fore.RED + "Well, maybe the questions you recieved were particularly difficult! Or maybe trivia just isn't your thing. Nice try!")
 
-print(percentageScore)
+#Game loop
+while True:
+    #Functions to run the game code
+    endScreen(runGame())
+    #Ask if user wants to play again
+    loopInput = str(input(Fore.YELLOW + "Play again? Empty answer will be considered as no: "))
 
-#get percentage by dividing playerscore with QUESTION_AMOUNT
+    if loopInput.strip().lower() in ["true", "yes", "1", "y", "yeah", "sure"]:
+        print(Fore.YELLOW + "Restarting game...")
+        input(Fore.LIGHTBLACK_EX + "Press Enter to continue...")
+    else:
+        break
 
-print(f"You have answered all the questions. \n Your total score is: {playerScore} out of {QUESTION_AMOUNT}")
-if percentageScore == 100:
-    print("Wow, that is impressive! You answered every question correctly.")
-elif percentageScore >= 80:
-    print("Pretty good! You got the majority of questions correct.")
-elif percentageScore >= 60:
-    print("Not bad! You've answered more questions correctly than incorrectly.")
-elif percentageScore >= 40:
-    print("Not the best score, but there is definitely room for improvement!")
-else:
-    print("Well, maybe the questions you recieved were particularly difficult! Or maybe trivia just isn't your thing. Nice try!")
-
+#Final message before program terminates
+print(Fore.WHITE + "Thanks for playing!")
 
 
 
